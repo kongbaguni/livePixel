283b3e45a50f5ed12ae95245a3ec7f0e40cd4417
chang-yul, Seo <kongbaguni@gmail.com>
2023-08-21 13:33:32 +0900

    Initial Commit

---
+++ LivePixel/LivePixelApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivePixelApp.swift
+//  LivePixel
+//
+//  Created by 서창열 on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct LivePixelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
